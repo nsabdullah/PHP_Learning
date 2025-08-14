85f490dd85cf94be73cc7e9aea81f27b2d2fe3f2
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2025-08-14 18:37:26 +0600

    php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+//echo  "hello  world  I llike you" . "<br>" ;
+
+// echo  "I  love my country";
+ 
+
+// $a  = 7;
+// $c = 18;
+
+// echo  "My  age is $c and i have $a  students"
+$a = $GLOBALS("HTTP_HOST")
+
+echo $a  ;  
+
+?>  
